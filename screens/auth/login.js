d4664b380e02da8cb5bdff74e15fb52374ee9bd4
Ceo-Sammarco <noah.kd@gmail.com>
2024-10-03 16:37:46 -0300

    @Ceo-Sammarco - qui 03 out 24

---
+++ screens/auth/login.js
@@ -0,0 +1,18 @@
+
+
+// const 
+//    LoginView = `<p>login</p>`
+// ;
+const 
+   style = `<style>
+      #p {
+         color: "#fc0";
+      }
+   </style>`
+   ,
+
+   LoginView = `
+      ${ style }
+      <p id="p">Login View</p>
+   `
+;
