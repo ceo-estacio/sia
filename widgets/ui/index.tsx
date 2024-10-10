a721e8b0df45a7c8eb4f8355683f95c30a855322
Ceo-Sammarco <noah.kd@gmail.com>
2024-10-10 13:25:18 -0300

    @Ceo-Sammarco - qui 10 out 24

--- widgets/ui/index.tsx
+++ widgets/ui/index.tsx
@@ -10,6 +10,7 @@ import {
    Image,
    ScrollView,
    Pressable,
+   Dimensions,
 } from "react-native";
 import _, { Code, Header, HomePage, Page, Picture, Pix, Section } from "@/widgets/elements";
 import { Href, router } from "expo-router";
@@ -58,54 +59,63 @@ export function DisciplinasMenu( { ...props }: DisciplinasMenuProps ) {
 
    return( <>
       <HomePage>
-         <ScrollView style={{ backgroundColor: Palette.dark[2] }}>
-            {/* <Header bg="#00559c" h={129} ph={18} pv={18} centerH> */}
+         {/* <ScrollView style={{ backgroundColor: Palette.dark[2] }}> */}
+         <ScrollView style={{ backgroundColor: Palette.dark[1] }}>
             <Header bg={ Palette.dark[1] } h={129} ph={18} pv={18} centerH>
                <_.H4 style={{ color: "#ccc" }}>Disciplina</_.H4>
                <_.H1 style={{ color: props.titleColor || "#eee" }}>{ props.title }</_.H1>
             </Header>
 
-            <View style={{ padding: 18, }}>
-               <Section pd={ 18 } 
-                  bg={ Palette.dark[1] }
-                  style={{
-                     borderRadius: 13,
-                     borderColor: "#9995",
-                     borderWidth: 1,
-                  }}
-               >
-                  {
-                     props.items && props.items.map( ( item, i ) => <>
-                        <Tema tema={ item.id } name={ item.title } key={ item.id }
-                           link={ item.link }
-                        />
-                        <View style={{ height: 1, backgroundColor: "#9995", }}/>
-                     </> )
-                     // [
-                     //    { 
-                     //       id: 2,
-                     //       title: "Estruturas de Dados Heterogêneas", 
-                     //       link: "/(disciplinas)/estruturaDeDados/tema-2", 
-                     //    },
-                     //    { 
-                     //       id: 3,
-                     //       title: "Modularização", 
-                     //       link: "/(disciplinas)/estruturaDeDados/tema-3", 
-                     //    },
-                     //    { 
-                     //       id: 4,
-                     //       title: "Listas, Pilhas, Filas e Deques", 
-                     //       link: "/(disciplinas)/estruturaDeDados/tema-4", 
-                     //    },
-                     // ].map( ( item, i ) => <>
-                     //    <Tema tema={ item.id } name={ item.title } key={ item.id }
-                     //       link={ item.link }
-                     //    />
-                     //    <View style={{ height: 1, backgroundColor: "#9995", }}/>
-                     // </> )
-                  }
-         
-               </Section>
+            <View style={{ 
+               backgroundColor: Palette.dark[2],
+               flex: 1, 
+               minHeight: (Dimensions.get( "window" ).height) - ( 129 - 30 ), 
+               borderTopStartRadius: 24,
+               }}
+            >
+
+               <View style={{ padding: 18, }}>
+                  <Section pd={ 18 } 
+                     bg={ Palette.dark[1] }
+                     style={{
+                        borderRadius: 13,
+                        borderColor: "#9995",
+                        borderWidth: 1,
+                     }}
+                  >
+                     {
+                        props.items && props.items.map( ( item, i ) => <>
+                           <Tema tema={ item.id } name={ item.title } key={ item.id }
+                              link={ item.link }
+                           />
+                           <View style={{ height: 1, backgroundColor: "#9995", }}/>
+                        </> )
+                        // [
+                        //    { 
+                        //       id: 2,
+                        //       title: "Estruturas de Dados Heterogêneas", 
+                        //       link: "/(disciplinas)/estruturaDeDados/tema-2", 
+                        //    },
+                        //    { 
+                        //       id: 3,
+                        //       title: "Modularização", 
+                        //       link: "/(disciplinas)/estruturaDeDados/tema-3", 
+                        //    },
+                        //    { 
+                        //       id: 4,
+                        //       title: "Listas, Pilhas, Filas e Deques", 
+                        //       link: "/(disciplinas)/estruturaDeDados/tema-4", 
+                        //    },
+                        // ].map( ( item, i ) => <>
+                        //    <Tema tema={ item.id } name={ item.title } key={ item.id }
+                        //       link={ item.link }
+                        //    />
+                        //    <View style={{ height: 1, backgroundColor: "#9995", }}/>
+                        // </> )
+                     }
+            
+                  </Section>
+               </View>
             </View>
          </ScrollView>
       </HomePage>
