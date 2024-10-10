2a7a5b92c2924b1e3be35919bd84945a3a013a22
Ceo-Sammarco <noah.kd@gmail.com>
2024-10-09 23:55:25 -0300

    @Ceo-Sammarco - qua 09 out 24

--- app/(routes)/(disciplinas)/estruturaDeDados/index.tsx
+++ app/(routes)/(disciplinas)/estruturaDeDados/index.tsx
@@ -51,20 +51,41 @@ export default function EstruturaDeDadosView( { ...props } ) {
                <_.H1 style={{ color: "#eee" }}>Estrutura de dados</_.H1>
             </Header>
 
-            <Section pd={ 18 }>
-
-               <Tema tema={2} name="Estruturas de Dados Heterogêneas" 
-                  link="/(disciplinas)/estruturaDeDados/tema-2"
-               />
-
-               <Tema tema={3} name="Modularização" 
-                  link="/(disciplinas)/estruturaDeDados/tema-3"
-               />
-               <Tema tema={4} name="Listas, Pilhas, Filas e Deques"
-                  link="/(disciplinas)/estruturaDeDados/tema-4"
-               />
-      
-            </Section>
+            <View style={{ padding: 18, }}>
+               <Section pd={ 18 } 
+                  style={{
+                     borderRadius: 13,
+                     borderColor: "#9995",
+                     borderWidth: 1,
+                  }}
+               >
+                  {
+                     [
+                        { 
+                           id: 2,
+                           title: "Estruturas de Dados Heterogêneas", 
+                           link: "/(disciplinas)/estruturaDeDados/tema-2", 
+                        },
+                        { 
+                           id: 3,
+                           title: "Modularização", 
+                           link: "/(disciplinas)/estruturaDeDados/tema-3", 
+                        },
+                        { 
+                           id: 4,
+                           title: "Listas, Pilhas, Filas e Deques", 
+                           link: "/(disciplinas)/estruturaDeDados/tema-4", 
+                        },
+                     ].map( ( item, i ) => <>
+                        <Tema tema={ item.id } name={ item.title } key={ item.id }
+                           link={ item.link }
+                        />
+                        <View style={{ height: 1, backgroundColor: "#9995", }}/>
+                     </> )
+                  }
+         
+               </Section>
+            </View>
          </ScrollView>
       </HomePage>
    </> );
