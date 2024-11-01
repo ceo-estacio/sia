83422261eb11aadb410d6b0ae4742de4c841a204
Ceo-Sammarco <noah.kd@gmail.com>
2024-11-01 13:27:24 -0300

    @Ceo-Sammarco - sex 01 nov 24

---
+++ constants/Sizes.ts
@@ -0,0 +1,26 @@
+
+
+export const sizes = {
+   Text: {
+      title: { 
+         size: 36, 
+         margin: [ , , 16 ] 
+      },
+      subtitle: { 
+         size: 32, 
+         margin: [ 38.56, 0, 8 ] 
+      },
+      sTitle: { 
+         size: 24, 
+         margin: [ 36, 0, 8 ]
+      },
+      text: { 
+         size: 14, 
+         margin: [ 14, 0, 0 ] 
+      },
+      list: {
+         size: 0,
+         margin: [ 0, 40, 14, 0 ]
+      }
+   }
+};
